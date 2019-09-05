71c7d038042695f92964e049817f138d6cac4013
Thomasgui <guilhem.thomas@gmail.com>
2019-09-05 15:15:47 +0200

    Repository

--- AgenceGraf/src/Controller/PropertyController.php
+++ AgenceGraf/src/Controller/PropertyController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\Yaml\Dumper;
 
 class PropertyController extends AbstractController
 {
+
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
     /**
      * @Route("/biens", name="property.index")
      *
      * @return Response
      */
     public function index(): Response {
+
+        $property = $this->repository->findAll();
+        Dump($property);
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties'
         ]);
