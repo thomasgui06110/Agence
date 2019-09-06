e353f038e63123c96ab068ba8382e532379f237b
Thomasgui <guilhem.thomas@gmail.com>
2019-09-06 15:49:17 +0200

    feat: validation des donnees

---
+++ AgenceGraf/src/Form/PropertyType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use PhpParser\Builder\Property as PhpParserProperty;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', null, [
+                'label' => 'Titre'
+            ])
+            ->add('description', null, [
+                'label' => 'Description'
+            ])
+            ->add('surface')
+            ->add('rooms', null, [
+                'label' => 'Pièces'
+            ])
+            ->add('bedrooms', null, [
+                'label' => 'Chambres'
+            ])
+            ->add('floor', null, [
+                'label' => 'Etage'
+            ])
+            ->add('price', null, [
+                'label' => 'Prix'
+            ])
+            ->add('heat', ChoiceType::class, [
+                'choices' => $this->GetChoices(),
+                'label' => 'Chauffage'
+            ])
+            ->add('city', null, [
+                'label' => 'Ville'
+            ])
+            ->add('address')
+            ->add('postal_code', null, [
+                'label' => 'CP'
+            ])
+            ->add('sold', null, [
+                'label' => 'Vendu'
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+        ]);
+    }
+
+    public function GetChoices()
+    {
+        $choices = Property::HEAT;
+        $output = [];
+        foreach($choices as $k => $v) {
+            $output[$v] = $k;
+        }
+        return $output;
+    }
+}
